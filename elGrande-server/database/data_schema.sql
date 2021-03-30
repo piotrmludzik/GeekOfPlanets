6c6cbedaa8b5fb775a6c71f09a78b99271943122
Magdalena Podolewska <magdalena@podolewska.com>
2021-03-30 11:56:11 +0200

    feat: data_schema.sql file and application.propperties created

---
+++ elGrande-server/database/data_schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "user" (
+                      id       SERIAL PRIMARY KEY NOT NULL,
+                      cell     CHAR(250) NOT NULL
+);
+
+CREATE TABLE user_registration_data (
+                        id        SERIAL PRIMARY KEY NOT NULL,
+                        user_id   SERIAL NOT NULL,
+                        login     CHAR(250) NOT NULL,
+                        password  CHAR(250) NOT NULL
+);
+
+ALTER TABLE ONLY user_registration_data ADD CONSTRAINT fk_genre_id
+    FOREIGN KEY (user_id) REFERENCES "user"(id);
