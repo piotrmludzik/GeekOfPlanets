aa69320e10d9bd7763de0f59f3fa72a8871ac27e
Piotr Młudzik <piotr.mludzik@gmail.com>
2021-04-01 14:25:53 +0200

    style: move the websocket config class to separate package

--- elGrande-server/src/main/java/com/codecool/elgrande/websocket/configurations/WebSocketConfig.java
+++ elGrande-server/src/main/java/com/codecool/elgrande/websocket/configurations/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.codecool.elgrande.websocket;
+package com.codecool.elgrande.websocket.configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
