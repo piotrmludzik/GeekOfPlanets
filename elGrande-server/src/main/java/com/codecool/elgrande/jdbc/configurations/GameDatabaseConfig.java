17118cc12c3bec07999a46aa8fcb998b22166c49
Piotr Młudzik <piotr.mludzik@gmail.com>
2021-04-07 13:24:14 +0200

    fix: primary annotation position

--- elGrande-server/src/main/java/com/codecool/elgrande/jdbc/configurations/GameDatabaseConfig.java
+++ elGrande-server/src/main/java/com/codecool/elgrande/jdbc/configurations/GameDatabaseConfig.java
@@ -11,11 +11,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import javax.sql.DataSource;
 
 @Configuration
-@Primary
 @PropertySource({"classpath:application.properties"})
 @EnableJpaRepositories(basePackages="com.codecool.elgrande.model.game")
 public class GameDatabaseConfig {
     @Bean(name="gameDB")
+    @Primary
     @ConfigurationProperties(prefix="spring.game-datasource")
     public DataSource gameDataSource() {
         return DataSourceBuilder.create().build();
