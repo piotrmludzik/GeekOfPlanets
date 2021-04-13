225e2d7938ab890f0e0f88b090a30b5e62daf3e4
Magdalena Podolewska <magdalena@podolewska.com>
2021-04-13 18:43:12 +0200

    FEAT: changePlayerName method added to PlayerController

--- elGrande-server/src/main/java/com/codecool/elgrande/controller/PlayerController.java
+++ elGrande-server/src/main/java/com/codecool/elgrande/controller/PlayerController.java
@@ -1,10 +1,12 @@
 package com.codecool.elgrande.controller;
 
 import com.codecool.elgrande.dto.MessageDto;
+import com.codecool.elgrande.game.jdbc.service.PlayerService;
 import com.codecool.elgrande.game.logic.Direction;
 import com.codecool.elgrande.game.logic.GameLogic;
 import com.codecool.elgrande.game.model.actors.Player;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,10 +14,12 @@ public class PlayerController {
 
     private final GameLogic gameLogic;
     private final Player player;
+    private final PlayerService playerService;
 
-    public PlayerController(GameLogic gameLogic, Player player) {
+    public PlayerController(GameLogic gameLogic, Player player, PlayerService playerService) {
         this.gameLogic = gameLogic;
         this.player = player;
+        this.playerService = playerService;
     }
 
     @GetMapping("/game")
@@ -23,5 +27,11 @@ public class PlayerController {
         int id = player.getId();
         gameLogic.movePlayer(id, Direction.EAST);
         return new MessageDto("Player on the board");
+    }
+
+    @PostMapping("/dashboard")
+    public MessageDto changePlayerName(String newName) {
+        playerService.changePlayerName(newName);
+        return new MessageDto("Player's dashboard'");
     }
 }
